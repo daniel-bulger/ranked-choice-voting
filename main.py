d170d248b93f59e762b9d11e4aaf2e0837c7f2df
Daniel Bulger <dpbulger@gmail.com>
2025-05-17 20:03:57 -0400

    Handle error where a movie with an unknown name wins the election.
    Mostly helpful for debugging so we can see what id is being looked up and then i can look it up in the db

--- main.py
+++ main.py
@@ -350,6 +350,9 @@ def get_instant_runoff_winner_ids(preferences, current_user_id = 0):
     # Run the Instant Runoff Voting (IRV) election
     return run_instant_runoff_election(user_preference_list)
 
+def get_movie_name(id, id_to_name):
+    return id_to_name.get(id,"Unknown movie with id {0}".format(x))
+
 def get_instant_runoff_winners(preferences,current_user_id = 0):
     election_result = get_instant_runoff_winner_ids(preferences,current_user_id)
     # Retrieve all the movies
@@ -360,7 +363,7 @@ def get_instant_runoff_winners(preferences,current_user_id = 0):
         id_to_name[movie.id] = movie.title
 
     print(election_result)
-    ranked_names = ([id_to_name[x] for x in election_result[1]],[{id_to_name[k]:v for k,v in d.items()} for d in election_result[0]],[[id_to_name[x] for x in y] for y in election_result[2]])
+    ranked_names = ([get_movie_name(x,id_to_name) for x in election_result[1]],[{get_movie_name(k,id_to_name):v for k,v in d.items()} for d in election_result[0]],[[get_movie_name(x,id_to_name) for x in y] for y in election_result[2]])
     return ranked_names
 
 def find_condorcet_winners(preferences):
